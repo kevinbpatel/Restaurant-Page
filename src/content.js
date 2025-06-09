3cd0f5972677d0740eaf0eeda868519bef8539a8
kevinbpatel <kevnpatel99@gmail.com>
2025-06-09 15:31:37 -0400

    Remove HTML content and add through javascript

---
+++ src/content.js
@@ -0,0 +1,19 @@
+
+
+export const displayContent = () => { 
+  const content = document.querySelector("#content");
+
+  const img = document.createElement("img");
+
+  img.src = "how-to-grill-chicken-breast-pic.jpg";
+  img.width = 500;
+
+  const tagLine =  document.createElement("h2");
+  const description = document.createElement("div");
+  tagLine.textContent = "Delicious Food stays within your calories and macros.";
+  description.textContent = "KP's Gym Food is a game-changer for anyone who loves delicious meals but wants to stay on track with their fitness goals. Every dish is crafted with care, balancing flavor and nutrition so you never have to choose between eating well and enjoying your food. Whether you're fueling up before a workout or winding down after a long day, KP's Gym Food offers high-quality ingredients, perfectly portioned meals, and a menu designed to keep you within your calories and macros. It's not just food—it's fuel for your best self!";
+
+  content.appendChild(img);
+  content.appendChild(tagLine);
+  content.appendChild(description);
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import { displayContent } from "./content.js";
+
+displayContent();
 
 console.log(greeting);
